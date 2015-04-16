c35ecb9eea433b50f0b7c7ea07c77f20d233ffbb
Christopher Treviño <christophertrev@gmail.com>
2015-04-15 17:52:23 -0700

    Add first solution

--- projectEuler/problem3.js
+++ projectEuler/problem3.js
@@ -7,24 +7,40 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 */
 
-//this problem is basicaly find all primes of a number. 
+//this problem is basicaly find all primes factors of a number. 
 
 //Naive solution
 
 
 
-function findLargestPrime(number){
+function findLargestPrimeFactor(number){
 
   //find all factors
   //max factor is its square root
   var squareRoot = Math.sqrt(number);
-  var factors = [];
-  for(var i = 2 ; i< squareRoot; i++){
+  console.log('squareRoot', squareRoot);
+  var largestPrime = 0 ;
+  // var smallfactors = [];
+  // var largefactors = [];
+  for(var i = 1 ; i<= squareRoot; i++){
     console.log('current number', i);
     console.log('number divided by cur', number/i);
-    if(isInteger(number / i )){
-      factors.unshift(i);
-      factors.push(number/i);
+    var currentOtherFactor = number/ i; 
+    if(isInteger(currentOtherFactor)){
+      //if the conjugate Factor is an Integer it is a properfactor
+      // console.log(largestPrime < (currentOtherFactor))
+      if( isPrime(i)  && largestPrime < i ){
+        largestPrime = i;
+      }
+
+
+      if( isPrime (currentOtherFactor) && largestPrime < (currentOtherFactor) ){
+        largestPrime = currentOtherFactor 
+      }
+
+
+      // smallfactors.push(i);
+      // largefactors.unshift(currentOtherFactor);
     }
   }
 
@@ -32,9 +48,24 @@ function findLargestPrime(number){
     return number === Math.floor(number);
   }
 
-  return factors
+  function isPrime(n){
+    var sqrt = Math.sqrt(n)
+    if(isInteger(sqrt)){
+      return false;
+    }
+    for(var i = 2; i < sqrt; i ++){
+      if(isInteger(n/i)){
+        return false;
+      }
+    }
+    return true;
+
+  }
+  // console.log(smallfactors, largefactors);
+  // console.log(largestPrime);
+  return largestPrime;
 
 }
 // Math.sqrt();
 
-console.log(findLargestPrime(100));
+console.log(findLargestPrimeFactor(100));
