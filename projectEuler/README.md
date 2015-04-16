4f0d83d28be2b1646e22953d3abd324b8ef46529
Christopher Treviño <christophertrev@gmail.com>
2015-04-16 13:20:16 -0700

    Add incomplete solution to problem 5

--- projectEuler/README.md
+++ projectEuler/README.md
@@ -1,3 +1,5 @@
 Outstanding problems 
 Problem 3 
   Soultion takes too long please refactor
+Problem 5 
+  Incorrectt solution 
--- projectEuler/problem5.js
+++ projectEuler/problem5.js
@@ -4,3 +4,64 @@ Problem 5
 
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 */
+
+//it will ahve all the primes  form 1 to maxNum
+//dont need 1 
+function evenlydivisibleBy(maxNum){
+  //start at max if sum is not divisible by the next nubmer multiply
+  // this straggey does not work it duplicates factors 
+  maxNum = 10;
+  // var sum = maxNum;
+  // for (var i = maxNum -1 ; i > 2 ; i--){
+  //   var remainder = sum % i;
+  //   console.log(i , sum);
+  //   console.log(sum % i);
+  //   if( remainder !== 0 ){
+
+  //     sum *= i;
+  //     // console.log(sum % (sum % i ));
+
+  //     // console.log( i , sum)
+  //   }
+  // } 
+  // return sum
+
+  // start with 1 
+  var sum = 2; 
+  for ( var i = 3 ; i <= maxNum; i++ ){
+    // if( sum % i )
+    // console.log(sum % i)
+    console.log( 'old sum', sum)
+    console.log( 'i ', i);
+    var remainder = sum % i;
+    console.log('remainder ', remainder)
+    if(remainder !== 0){
+
+      console.log( i % remainder)
+      if( i % remainder === 0 ){
+        sum *= remainder
+      } else {
+        sum *= i
+
+      }
+
+    }
+    console.log('new sum ', sum)
+  }
+
+  return sum
+
+}
+
+
+
+// console.log(5* 2 * 3 * 3 * 4 * 7 );
+// console.log(10 * 9 * 8 )
+// console.log(10 * 9 * 8 * 7 )
+// console.log(2 * 3 * 2 * 5 * 7 * 2 * 3 ) 
+// console.log(4 % remainder)
+console.log( 2 * 3 * 2 * 5 * 7 * 2)
+
+console.log(evenlydivisibleBy(10))
+
+// console.log( 720 % 3 )
