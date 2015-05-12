1369b986d31173832f64ee87b0ad99d02a239db6
Christopher Treviño <christophertrev@gmail.com>
2015-05-12 11:44:38 -0700

    JS: Add problem6 solution

--- projectEuler/problem6.js
+++ projectEuler/problem6.js
@@ -10,3 +10,27 @@ Hence the difference between the sum of the squares of the first ten natural num
 
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 */
+
+
+var sumOfSquares = function (n){
+  var sum = 0; 
+  for (var i = 1; i<= n; i++){
+    sum += i*i;
+
+  }
+  return sum;
+}
+
+
+var squareOfSums = function (n){
+  var sum = 0; 
+  for (var i = 1; i <= n; i++){
+    sum += i;
+  }
+  return sum*sum;
+}
+
+
+console.log(squareOfSums(10) - sumOfSquares(10));
+console.log(squareOfSums(100) - sumOfSquares(10));
+// console.log(squareOfSums(100));
