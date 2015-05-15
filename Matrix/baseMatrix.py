933a5c4bfe44aef04316adfb8afdf6e373f85e3b
Christopher Treviño <christophertrev@gmail.com>
2015-05-15 15:54:46 -0700

    PY: add matrix class

--- Matrix/baseMatrix.py
+++ Matrix/baseMatrix.py
@@ -8,4 +8,31 @@ class baseMatrix():
       m = n
     self.n = n
     self.m = m
+    self._storage = {}
+
+  def __getitem__(self,key):
+    self.checkInBounds(key)
+    if key in self._storage:
+      return self._storage[key]
+    else: 
+      return 0
     
+  def __setitem__(self, key, value):
+    self.checkInBounds(key)
+    self._storage[key] = value
+
+
+  def checkInBounds(self, key):
+    if key[0] < 0 or key[1] < 0:
+      raise IndexError('index Out of bounds')
+    if key[0] >= self.n or key[1] >= self.m:
+      raise IndexError('index Out of bounds')
+
+
+
+if __name__ == '__main__':
+  mat = baseMatrix(2)
+  mat[0,1] = 1
+  mat[0,0] = 2
+  print mat._storage
+  print mat.__class__.__name__
--- Matrix/baseMatrix.test.py
+++ Matrix/baseMatrix.test.py
@@ -5,8 +5,7 @@ from baseMatrix import *
 class TestBaseMatrix(unittest.TestCase):
   
   def setUp(self):
-    # self.testMatrix = baseMatrix(1,3)
-    pass
+    self.testMatrix = baseMatrix(4)
 
   def testInstanciation(self):
     testMatrix  = baseMatrix(1,3)
@@ -18,9 +17,37 @@ class TestBaseMatrix(unittest.TestCase):
     self.assertEqual(testMatrix.n ,1)
     self.assertEqual(testMatrix.m ,1)
 
+  def testGetValueOfZeroFrom(self):
+    self.assertEqual(self.testMatrix[0,1], 0)
 
+  def testGetThrowOutOFBoundsError(self):
+    with self.assertRaises(IndexError):
+      self.testMatrix[-1,1]
+    with self.assertRaises(IndexError):
+      self.testMatrix[0,-1]
+    with self.assertRaises(IndexError):
+      self.testMatrix[10,1]
+    with self.assertRaises(IndexError):
+      self.testMatrix[1,10]
 
+  def testReturnSetValue(self):
+    testMatrix = baseMatrix(4)
+    saveValue = 1 
+    testMatrix[0,0] = saveValue
+    self.assertEqual(testMatrix[0,0], saveValue)
 
+  def testSetThrowOutOFBoundsError(self):
+    with self.assertRaises(IndexError):
+      self.testMatrix[-1,1] = 1
+    with self.assertRaises(IndexError):
+      self.testMatrix[0,-1] = 2
+    with self.assertRaises(IndexError):
+      self.testMatrix[10,1] = 2
+    with self.assertRaises(IndexError):
+      self.testMatrix[1,10] = 1
+
+  def testStorageArrayIsInOrder(self): 
+    pass
 
 if __name__ == '__main__':
   unittest.main()
